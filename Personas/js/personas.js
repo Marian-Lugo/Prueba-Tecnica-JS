5da205b20c1ffb9a1d01ced89031c28950770a61
Marian Estigarribia <mnelugo@gmail.com>
2023-03-30 00:31:48 -0400

    Agregue funcion editar

--- Personas/js/personas.js
+++ Personas/js/personas.js
@@ -34,7 +34,7 @@ function listarPersonas() {
           '<button class="btn btn-danger" onclick="eliminarPersona(' +
           myItems[i].COD_PERSONA +
           ')">Borrar</button>' +
-          '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#detalleModal" onclick="detallesPersona(' +
+          '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#detalleModal" onclick="cargarFormulario(' +
           myItems[i].COD_PERSONA +
           ')">Detalles</button>' +
           "</td>" +
@@ -88,97 +88,81 @@ function eliminarPersona(idPersona) {
 }
 
 function cargarFormulario(idPersona) {
-  $.ajax({
-    url: urlRest + "/" + idPersona,
-    type: "GET",
-    headers: {
-      Token: "codigoToken",
-      ApiKey: "codigoApikey",
-    },
-    datatype: "JSON",
-    success: function (response) {
-      var myItem = response.items[0];
-      $("#codigo").val(myItem.COD_PERSONA);
-      $("#nombre").val(myItem.NOMBRE);
-      $("#apellido").val(myItem.APELLIDO);
-      $("#nroDocumento").val(myItem.NRO_DOCUMENTO);
+  let id=idPersona;
+    $.ajax({
+        url: "https://telemedicina.jakemate.net:7141/api/webservice/metodo",
+        data: { _nombreMetodo_: "listarPersonas" },
+        method: "POST",
+        headers: {
+          Token: "NJKJNTL8SNKH5JJRTS32ZGSIIDPGHLU6KRXLQMLMJJU8MD7EY5TSWMGD2D6Z",
+          ApiKey: "ISSTIXZTV53RZURJKTZD3MXVMEW7X3",
+        },
+        success: function (resultado) {
+            let myItems = resultado.resultado.Table;
+            let nombre = "";
+            let apellido="";
+            let nroDocumento=0;
+            let valor="";
+            for (i = 0; i < myItems.length; i++) {
+                if (myItems[i].COD_PERSONA === id) {
+                    nombre=myItems[i].NOMBRE;
+                    apellido=myItems[i].APELLIDO;
+                    nroDocumento=myItems[i].NRO_DOCUMENTO;
+                }
+            }
+            valor +=`<form id="formularioPersona">
+            <div class="form-group">
+              <label for="formGroupExampleInput2">Nombre</label>
+              <input id="nombreEditado" type="text" class="form-control" id="formGroupExampleInput2" value="${nombre}" required>
+            </div>
+            <div class="form-group">
+              <label for="formGroupExampleInput2">Apellido</label>
+              <input id="apellidoEditado" type="text" class="form-control" id="formGroupExampleInput2" value="${apellido}" required>
+            </div>
+            <div class="form-group">
+              <label for="formGroupExampleInput2">Nro. Documento</label>
+              <input id="nroDocumentoEditado" type="text" class="form-control" id="formGroupExampleInput2" value="${nroDocumento}" required>
+            </div>
+            <button
+                type="button"
+                class="btn btn-secondary"
+                data-dismiss="modal"
+                onclick="actualizarPersona(${id})"
+              >
+                Aceptar
+              </button>
+        </form>`;
 
-      var valor =
-        '<input id="btnActualizar" type="submit" onclick="actualizarPersona(' +
-        myItem.id +
-        ')" value="Actualizar" class="btn btn-warning">';
-      $("#btnFormulario").html(valor);
+        $("#detallesPersona").html(valor);
 
-      $("#codigo").prop("disabled", true);
-    },
-  });
+        },
+      });
+  
 }
 
-function detallesPersona(idPersona) {
-  $.ajax({
-    url: urlRest + "/" + idPersona,
-    type: "GET",
-    headers: {
-      Token: "codigoToken",
-      ApiKey: "codigoApikey",
-    },
-    datatype: "JSON",
-    success: function (response) {
-      let myItem = response.items[0];
-      let valor =
-        "<strong>Codigo:</strong> " +
-        myItem.COD_PERSONA +
-        " <br>" +
-        "<strong>Nombre:</strong> " +
-        myItem.NOMBRE +
-        " <br>" +
-        "<strong>Apellido:</strong> " +
-        myItem.APELLIDO +
-        " <br>" +
-        "<strong>Nro. Documento:</strong> " +
-        myItem.NRO_DOCUMENTO +
-        " <br>" +
-        '<button class="btn btn-warning" onclick="cargarFormulario(' +
-        myItem.COD_PERSONA +
-        ')">Editar</button>';
 
-      $("#detallesPersona").html(valor);
-      $("#detallesPersona").show();
-    },
-  });
-}
 
-function actualizarPersona(idPersona) {
-  let datosFormulario = {
-    id: idPersona,
-    nombre: $("#nombre").val(),
-    apellido: $("#apellido").val(),
-    nroDocumento: $("#nroDocumento").val(),
-  };
+function actualizarPersona(id) {
+  let nombre= $("#nombreEditado").val();
+  let apellido= $("#apellidoEditado").val();
+  let nroDocumento= $("#nroDocumentoEditado").val();
 
-  let datosFormularioJson = JSON.stringify(datosFormulario);
+  console.log("DATOS A EDITAR", nombre, apellido, nroDocumento);
 
   $.ajax({
-    url: urlRest,
-    type: "PUT",
+    url: "https://telemedicina.jakemate.net:7141/api/webservice/metodo",
+    data: {  _nombreMetodo_: "editarPersona", COD_PERSONA: id, NOMBRE: nombre, APELLIDO: apellido, NRO_DOCUMENTO: nroDocumento},
+    method: "POST",
     headers: {
-      Token: "codigoToken",
-      ApiKey: "codigoApikey",
+      Token: "NJKJNTL8SNKH5JJRTS32ZGSIIDPGHLU6KRXLQMLMJJU8MD7EY5TSWMGD2D6Z",
+      ApiKey: "ISSTIXZTV53RZURJKTZD3MXVMEW7X3",
     },
-    data: datosFormularioJson,
-    datatype: "JSON",
-    contentType: "application/json",
-    success: function (response) {
-      console.log(response);
-      listarPersonas();
+    success: function (resultado) {
+      console.log(resultado);
+      listarPersonas(); 
       limpiarFormulario();
     },
   });
-  let valor =
-    '<input id="btnCrear" type="submit" onclick="registrarPersona()" value="Crear" class="btn btn-primary"> ';
-  $("#btnFormulario").html(valor);
-  $("#codigo").prop("disabled", false);
-  $("#detallesPersona").hide();
 }
 
 function limpiarFormulario() {
